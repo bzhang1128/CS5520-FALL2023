ebadbe2eba0a718fa8d6389c5ddfcfdf6e4b99d1
bzhang1128 <bzhang73@asu.edu>
2023-09-28 15:10:42 -0700

    add goalitem

--- myApp/App.js
+++ myApp/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
-import Header from './components/Header';
-import { useState } from 'react';
-import Input from './components/input';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import Header from "./components/Header";
+import { useState } from "react";
+import Input from "./components/Input";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [goals, setGoals] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const name = "My Personal App";
-  //update this callback function to receive the changed text and store it in text state
-  
-  function changeDataHandler(data) {
-    console.log("callback function called", data);
+  const name = "My Awesome App";
+
+  function changedDataHandler(data) {
+    console.log("callback function called ", data);
+    const newGoal = { text: data, id: Math.random() };
+    // const newArray = [...goals, newGoal];
+    // setGoals(newArray)
+    setGoals((prevGoals) => {
+      return [...prevGoals, newGoal];
+    });
+    //use the received data to update the text state variable
     setText(data);
     makeModalInvisible();
   }
+
   function makeModalVisible() {
     setIsModalVisible(true);
   }
@@ -23,25 +41,50 @@ export default function App() {
     setIsModalVisible(false);
   }
 
+  function goalDeleteHandler(deletedId) {
+    console.log("I was deleted ", deletedId);
+    //use array.filter to remove the item with deletedId
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Text>Open up App.js to start working on {name}!</Text> */}
-      {/* render header component here and pass a props with the name variable as value */}
+      {/* <Text>Open up App.js to start working on {name} !</Text> */}
+      {/* render Header component here and pass a prop to it with the name variable as value */}
       <View style={styles.topContainer}>
-        <Header appName={name}/>
+        <Header appName={name} />
         <StatusBar style="auto" />
-        <Input 
-          changedHandler={changeDataHandler}
-          modalVisibility={isModalVisible}
+        {/* pass another prop to Input with the modalIsVisible as its value */}
+        <Input
+          changedHandler={changedDataHandler}
+          modalVisiblity={isModalVisible}
           hideModal={makeModalInvisible}
         />
-        <Button 
-          title="Add a goal" 
-          onPress={() => makeModalVisible()} 
-        />
+        <Button title="Add a goal" onPress={makeModalVisible} />
+        {/* Inside this text show what user is typing */}
       </View>
       <View style={styles.bottomContainer}>
-        <Text>{text}</Text>
+        {/* <Text style={styles.text}>{text}</Text> */}
+        {/* <ScrollView
+          bounces={false}
+          contentContainerStyle={styles.contentContainerStyle}
+        >
+          {goals.map((goal) => {
+            return (
+              <Text key={goal.id} style={styles.text}>
+                {goal.text}
+              </Text>
+            );
+          })}
+        </ScrollView> */}
+        <FlatList
+          contentContainerStyle={styles.contentContainerStyle}
+          data={goals}
+          renderItem={({ item }) => {
+            return <GoalItem goal={item} deleteHandler={goalDeleteHandler} />;
+
+            // return <Text style={styles.text}>{item.text}</Text>;
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -50,19 +93,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
   },
   topContainer: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
   },
   bottomContainer: {
     flex: 4,
-    backgroundColor: '#a59',
+    backgroundColor: "#dcd",
+  },
+  contentContainerStyle: {
     alignItems: "center",
-    justifyContent: "center",
-  }
+  },
+  text: {
+    color: "#a09",
+    backgroundColor: "#aaa",
+    borderRadius: 5,
+    padding: 5,
+    fontSize: 30,
+    overflow: "hidden",
+    marginBottom: 20,
+  },
 });
