7e4f510f0cca319f857be591b3a0af6b232d9744
bzhang1128 <bzhang73@asu.edu>
2023-09-27 19:39:09 -0700

    add explaination comments for functions in validation.js

--- CS5520 Mobile/Assignment1/utilities/validation.js
+++ CS5520 Mobile/Assignment1/utilities/validation.js
@@ -1,11 +1,14 @@
+// Validate the name input is non-numeric and the length of the input is more than 1 character
 const isNameValid = (name) => {
   return /^[A-Za-z\s]+$/.test(name) && name.length > 1;
 };
 
+// Validate the email address input
 const isEmailValid = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
+// Validate the phone number input (consists of 10 digits)
 const isPhoneValid = (phone) => {
   return /^\d{10}$/.test(phone);
 };
